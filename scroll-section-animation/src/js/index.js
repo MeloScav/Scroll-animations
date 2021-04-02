7a337ae78b18fe55bf66d76c19836c7f912264a9
Méloé <meloscavezzoni@gmail.com>
2021-04-02 11:09:24 +0200

    elements selection

---
+++ scroll-section-animation/src/js/index.js
@@ -0,0 +1,11 @@
+const project = document.querySelectorAll(".project");
+
+project.forEach(el => {
+    let overlay = el.querySelector(".overlay");
+    let projectInfo = el.querySelector(".project-info");
+    let smallTitle = el.querySelector(".small-title");
+    let projectLink = el.querySelector(".project-link");
+    let title = el.querySelector("h4");
+
+
+});
