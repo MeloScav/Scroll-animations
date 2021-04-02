6c48b51ced10b378827f774a66761f3197e2b220
Méloé <meloscavezzoni@gmail.com>
2021-04-02 11:53:49 +0200

    Animation

--- scroll-section-animation/src/js/index.js
+++ scroll-section-animation/src/js/index.js
@@ -1,4 +1,5 @@
 const project = document.querySelectorAll(".project");
+const controller = new ScrollMagic.Controller();
 
 project.forEach(el => {
     let overlay = el.querySelector(".overlay");
@@ -7,5 +8,23 @@ project.forEach(el => {
     let projectLink = el.querySelector(".project-link");
     let title = el.querySelector("h4");
 
+    // Animation
+    let TL = new TimelineMax();
+
+    TL
+    .fromTo(overlay, 2, {skewX: 30, scale: 1.5}, {skewX: 0, xPercent: 100, transformOrigin: "0% 100%", ease: Power4.easeOut})
+    .from(projectInfo, 1, {scaleY: 0, transformOrigin: "bottom left"}, "-=1.5")
+    .from(smallTitle, 0.3, {autoAlpha: 0, y: 30, ease: Power4.easeOut}, "-=0.8")
+    .from(projectLink, 0.3, {autoAlpha: 0, y: 30, ease: Power4.easeOut}, "-=0.8")
+    .from(title, 0.3, {autoAlpha: 0, y: 30, ease: Power4.easeOut}, "-=0.8")
+
+    // ScrollMagic
+    let scene = new ScrollMagic.Scene({
+        triggerElement: el
+    })
+    .addIndicators()
+    .setTween(TL)
+    .reverse(false)
+    .addTo(controller);
 
 });
